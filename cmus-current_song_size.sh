6966ed57350576a14e945ddb9364cf90ac91df19
gt <static.vortex@gmx.com>
2013-10-21 18:52:01 +0530

    cmus-current_song_size.sh: improve code formatting

--- cmus-current_song_size.sh
+++ cmus-current_song_size.sh
@@ -4,8 +4,8 @@
 # Requires zenity
 
 # show size and filename
-size=$(cmus-remote -Q | awk '/file/ {print $2}' | xargs du -h | awk '{print $1}')
-name=$(cmus-remote -Q | awk '/file/ {print $2}' | xargs basename)
+size=$(cmus-remote -Q | awk '/file/ { print $2 }' | xargs du -h | awk '{ print $1 }')
+name=$(cmus-remote -Q | awk '/file/ { print $2 }' | xargs basename)
 
 # zenity dialog
 zenity --title=cmus-filesize --timeout=5 --info --text="[${size}] - ${name}"
