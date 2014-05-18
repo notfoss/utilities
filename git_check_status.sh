c11862d7aed0514d2f7b46dc6bdaf9e0c35e2e53
gt <notfoss@gmx.com>
2014-05-18 23:24:42 +0530

    git_check_status.sh: several changes
    add a quiet output option while making verbose the default
    output. add option to list git repositories found under the
    current directory.

--- git_check_status.sh
+++ git_check_status.sh
@@ -7,12 +7,44 @@
 
 gitdirs=$(find "$PWD" -type d -name ".git" | xargs dirname)
 
-for dir in $gitdirs; do
-    cd $dir
+# list git repositories present under the current directory
+listdirs() {
+    echo -e "The following git repositories are present under the current directory:\n"
+    for dir in $gitdirs; do
+        echo "$dir"
+    done
+}
 
-    # show only those repositories which contain changes
-    sshort=$(git status --porcelain)
-    if [[ -n "$sshort" ]]; then
-        printf '\e[1;37m'"${dir}:"'\e[0m'" Changes present\n"
-    fi
-done
+quiet_output() {
+    for dir in $gitdirs; do
+        cd "$dir"
+        quiet_command=$(git status --porcelain)
+        if [[ -n "$quiet_command" ]]; then
+            printf '\e[1;37m'"${dir}:"'\e[0m'" Changes present\n"
+        fi
+    done
+}
+
+verbose_output() {
+    for dir in $gitdirs; do
+        cd "$dir"
+        quiet_command=$(git status --porcelain)
+        if [[ -n "$quiet_command" ]]; then
+            printf '\e[1;37m'"Repository ${dir}\n\n"'\e[0m'
+            git status
+            echo '--------------------'
+        fi
+    done
+}
+
+case $1 in
+    -l|--list-dirs)
+        listdirs
+        ;;
+    -q|--quiet)
+        quiet_output
+        ;;
+    *)
+        verbose_output
+        ;;
+esac
