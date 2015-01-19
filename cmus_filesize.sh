ce6a935d808f78a3a453ee75bceea82e4dd967e6
gt <notfoss@gmx.com>
2015-01-19 13:16:19 +0530

    cmus_filesize.sh: Put some checks in place
    * Dependency check for zenity.
    * Check if cmus is running or not.

--- cmus_filesize.sh
+++ cmus_filesize.sh
@@ -4,6 +4,16 @@
 # in a GUI dialog box.
 # Requires zenity.
 
+# Exit if zenity not present
+hash zenity > /dev/null 2>&1 || { echo -e "Zenity not installed. Please install it before continuing.\n"; exit 1; }
+
+cmus_running=$(cmus-remote > /dev/null 2>&1)
+
+if [[ "$?" -ne 0 ]]; then
+    echo -e "cmus not running. Exiting.\n"
+    exit 1
+fi
+
 # show size and filename
 size=$(cmus-remote -Q | awk '/file/ { print $2 }' | xargs du -h | awk '{ print $1 }')
 name=$(cmus-remote -Q | awk '/file/ { print $2 }' | xargs basename)
